0fd80cd8e4e6a8b8d17e4ede92352560b3dc70ae
ict283 <glennlum@icloud.com>
2021-06-19 11:53:14 +0800

    Add vector class

---
+++ Vector.h
@@ -0,0 +1,28 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+// The class declaration must have doxygen comments – put these in
+
+template <class T>
+class Vector
+{
+
+public:
+
+Vector::Vector();
+
+Vector::~Vector();
+
+//implement the rest of the methods
+
+private:
+
+T* list;
+
+};
+
+// class implementation follows with normal comments
+// you fill in the rest
+
+
+#endif
